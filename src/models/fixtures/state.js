0ac701de96bfc71552442a1dbe6c74b46bd1b795
remrobem <Rob.martin.5758@gmail.com>
2016-07-30 14:56:42 -0400

    Initial commit

---
+++ src/models/fixtures/state.js
@@ -0,0 +1,17 @@
+import fixture from 'can-fixture';
+
+const store = fixture.store([
+  { name: 'Ccalisota', short: 'CC' },
+  { name: 'New Troy', short: 'NT'}
+],{});
+
+fixture({
+  'GET /api/states': store.findAll,
+  'GET /api/states/{short}': store.findOne,
+  'POST /api/states': store.create,
+  'PUT /api/states/{short}': store.update,
+  'DELETE /api/states/{short}': store.destroy
+});
+
+export default store;
+
